bd5f0b2f76d3b15955055f384ac77276b60768dd
Ilya Kozak <kozak@tut.by>
2021-10-22 17:15:28 +0300

    feat: add cart-service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,13 +8,13 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://xc76c6s438.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://zz73b76oel.execute-api.eu-west-1.amazonaws.com/rds',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://ua69tkrc21.execute-api.eu-west-1.amazonaws.com/dev/api',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: true,
     bff: true,
-    cart: false,
+    cart: true,
   },
 };
